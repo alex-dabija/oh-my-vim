bf0672d06f6bda507eee000336a03ccebeeeff77
Alex Dabija <alex.dabija@gmail.com>
2021-03-22 20:30:49 +0100

    customize nvim-tree behaviour

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,6 +1,11 @@
 local M = {}
 
 function M.setup()
+  vim.g.nvim_tree_width = 40
+  vim.g.nvim_tree_follow = 1
+  vim.g.nvim_tree_auto_close = 1
+  vim.g.nvim_tree_indent_markers = 1
+
   vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
 end
 
