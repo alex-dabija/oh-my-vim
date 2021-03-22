90093b24814f90c0a438b0b4c27bd9e922bb3796
Alex Dabija <alex.dabija@gmail.com>
2021-03-22 21:12:14 +0100

    keyboard shortcuts for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,11 @@ function M.setup()
   vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', {noremap = true, silent = true})
   vim.api.nvim_set_keymap('n', '<Leader>fg', ':Telescope live_grep<CR>', {noremap = true, silent = true})
   vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope buffers<CR>', {noremap = true, silent = true})
+  vim.api.nvim_set_keymap('n', '<Leader>fh', ':Telescope help_tags<CR>', {noremap = true, silent = true})
+  vim.api.nvim_set_keymap('n', '<Leader>fm', ':Telescope marks<CR>', {noremap = true, silent = true})
+  vim.api.nvim_set_keymap('n', '<Leader>fr', ':Telescope registers<CR>', {noremap = true, silent = true})
+  vim.api.nvim_set_keymap('n', '<Leader>ft', ':Telescope treesitter<CR>', {noremap = true, silent = true})
+  vim.api.nvim_set_keymap('n', '<Leader>fo', ':Telescope vim_options<CR>', {noremap = true, silent = true})
 
   require('telescope').setup {
     defaults = {
