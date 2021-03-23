22ab2d7feb60b6ba2fbe0efd43a4fbecd5c3f283
Alex Dabija <alex.dabija@gmail.com>
2021-03-23 16:19:46 +0100

    configure lsp keyboard shortcuts

--- lua/plugins/lsp-config/init.lua
+++ lua/plugins/lsp-config/init.lua
@@ -3,7 +3,60 @@ local M = {}
 local lspconfig = require('lspconfig')
 
 function M.setup()
-  lspconfig.rust_analyzer.setup{}
+  configure_diagnostic_signs()
+
+  lspconfig.rust_analyzer.setup {
+    on_attach = configure_buffer,
+  }
+end
+
+function configure_diagnostic_signs()
+  configure_diagnostic_sign("LspDiagnosticsSignError", "")
+  configure_diagnostic_sign("LspDiagnosticsSignWarning", "")
+  configure_diagnostic_sign("LspDiagnosticsSignInformation", "")
+  configure_diagnostic_sign("LspDiagnosticsSignHint", "")
+end
+
+function configure_diagnostic_sign(sign_name, sign)
+  vim.fn.sign_define(sign_name, { texthl = sign_name, text = sign, numhl = sign_name })
+end
+
+function configure_buffer(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local opts = { noremap = true, silent = true }
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<Leader>D', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<Leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<Leader>e', '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '[d', '<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<Leader>q', '<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+  buf_set_keymap('n', '<Leader>wa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<Leader>wr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<Leader>wl', '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec([[
+      highlight LspReferenceRead cterm=bold ctermbg=red guibg=#464646
+      highlight LspReferenceText cterm=bold ctermbg=red guibg=#464646
+      highlight LspReferenceWrite cterm=bold ctermbg=red guibg=#464646
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]], false)
+  end
 end
 
 return M
