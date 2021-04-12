346018cea345cd8beb410ee2e9801d8d213b6c74
Alex Dabija <alex.dabija@gmail.com>
2021-04-12 15:37:17 +0200

    mark functions in utils.lua as local

--- lua/plugins/lsp-config/init.lua
+++ lua/plugins/lsp-config/init.lua
@@ -6,6 +6,53 @@ local virtual_text = require('plugins.lsp-config.virtual_text')
 local buffer = require('plugins.lsp-config.buffer')
 local keymaps = require('keymaps')
 
+local function configure_diagnostic_sign(sign_name, sign)
+  vim.fn.sign_define(sign_name, { texthl = sign_name, text = sign, numhl = sign_name })
+end
+
+local function configure_diagnostic_signs()
+  configure_diagnostic_sign("LspDiagnosticsSignError", "")
+  configure_diagnostic_sign("LspDiagnosticsSignWarning", "")
+  configure_diagnostic_sign("LspDiagnosticsSignInformation", "")
+  configure_diagnostic_sign("LspDiagnosticsSignHint", "")
+end
+
+local function configure_telescope_for_lsp(buf)
+  if utils.is_telescope_available() then
+    buf:set_keymaps(keymaps.LSP_TELESCOPE)
+  end
+end
+
+local function configure_buffer(client, bufnr)
+  local buf = buffer.new(client, bufnr)
+
+  virtual_text.on_attach(buf)
+
+  buf:set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  buf:set_keymaps(keymaps.LSP)
+
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec([[
+      highlight LspReferenceRead cterm=bold ctermbg=red guibg=#464646
+      highlight LspReferenceText cterm=bold ctermbg=red guibg=#464646
+      highlight LspReferenceWrite cterm=bold ctermbg=red guibg=#464646
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]], false)
+  end
+
+  configure_telescope_for_lsp(buf)
+end
+
+local function common_lsp_handlers()
+  return {
+    ["textDocument/publishDiagnostics"] = virtual_text.on_publish_diagnostics,
+  }
+end
+
 function M.setup()
   virtual_text.setup()
   configure_diagnostic_signs()
@@ -40,53 +87,6 @@ function M.setup()
         },
       },
     },
-  }
-end
-
-function configure_diagnostic_signs()
-  configure_diagnostic_sign("LspDiagnosticsSignError", "")
-  configure_diagnostic_sign("LspDiagnosticsSignWarning", "")
-  configure_diagnostic_sign("LspDiagnosticsSignInformation", "")
-  configure_diagnostic_sign("LspDiagnosticsSignHint", "")
-end
-
-function configure_diagnostic_sign(sign_name, sign)
-  vim.fn.sign_define(sign_name, { texthl = sign_name, text = sign, numhl = sign_name })
-end
-
-function configure_buffer(client, bufnr)
-  local buf = buffer.new(client, bufnr)
-
-  virtual_text.on_attach(buf)
-
-  buf:set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-  buf:set_keymaps(keymaps.LSP)
-
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
-      highlight LspReferenceRead cterm=bold ctermbg=red guibg=#464646
-      highlight LspReferenceText cterm=bold ctermbg=red guibg=#464646
-      highlight LspReferenceWrite cterm=bold ctermbg=red guibg=#464646
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]], false)
-  end
-
-  configure_telescope_for_lsp(buf)
-end
-
-function configure_telescope_for_lsp(buf)
-  if utils.is_telescope_available() then
-    buf:set_keymaps(keymaps.LSP_TELESCOPE)
-  end
-end
-
-function common_lsp_handlers()
-  return {
-    ["textDocument/publishDiagnostics"] = virtual_text.on_publish_diagnostics,
   }
 end
 
