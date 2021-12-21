1011df3855419d1c1207b36903c591b8558e582f
Alex Dabija <alex.dabija@gmail.com>
2021-12-21 15:50:58 +0100

    configure vim-oscyank to yank to remote clipboard

--- lua/plugins/oscyank.lua
+++ lua/plugins/oscyank.lua
@@ -1,6 +1,12 @@
 local M = {}
 
 function M.setup()
+  vim.api.nvim_exec([[
+    augroup osc_yank
+      autocmd!
+      autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankReg "' | endif
+    augroup end
+  ]], false)
 end
 
 return M
