a116ecc01bd033d09912c6a4590990ed5ada5b59
Alex Dabija <alex.dabija@gmail.com>
2021-12-21 15:20:23 +0100

    update nvim tree configuration

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -4,9 +4,14 @@ local keymaps = require('keymaps')
 
 function M.setup()
   vim.g.nvim_tree_width = 40
-  vim.g.nvim_tree_follow = 1
-  vim.g.nvim_tree_auto_close = 1
   vim.g.nvim_tree_indent_markers = 1
+
+  require('nvim-tree').setup {
+    auto_close = true,
+    update_focused_file = {
+      enable = true,
+    }
+  }
 
   keymaps.set_keymaps(keymaps.NVIM_TREE)
 end
