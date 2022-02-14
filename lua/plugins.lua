b9cd9c9196ec71c1578c5be5d7cd6866def4af9f
Alex Dabija <alex.dabija@gmail.com>
2022-02-14 16:38:16 +0100

    move nvim-cmp configuration to after directory

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,9 +15,6 @@ packer.startup(function(use)
   -- LSP
   use {
     'neovim/nvim-lspconfig',
-    --'git@git.tools.kbee.xyz:opensource/vim/nvim-lspconfig.git',
-    as = 'nvim-lspconfig',
-    -- branch = 'fix-lsp-new-file',
     config = function() require('plugins.lsp-config').setup() end
   }
   use {
@@ -31,10 +28,7 @@ packer.startup(function(use)
   }
 
   -- Autocomplete
-  use {
-    'hrsh7th/nvim-cmp',
-    config = function() require('plugins.nvim-cmp').setup() end,
-  }
+  use {'hrsh7th/nvim-cmp'}
   use {'hrsh7th/cmp-nvim-lsp'}
 
   -- Snippets
