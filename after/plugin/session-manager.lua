bbc6087de2be92de605989eff270f14d2ef5de85
Alex Dabija <alex.dabija@gmail.com>
2022-03-18 14:24:34 +0100

    session manager do not ignore not normal buffers

--- after/plugin/session-manager.lua
+++ after/plugin/session-manager.lua
@@ -13,7 +13,7 @@ require('session_manager').setup({
   colon_replacer = '++',
   autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
   autosave_last_session = true,
-  autosave_ignore_not_normal = true,
+  autosave_ignore_not_normal = false,
   autosave_ignore_filetypes = {
     'gitcommit',
   },
