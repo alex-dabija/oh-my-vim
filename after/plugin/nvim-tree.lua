b62237bdda39279b8031fa0303e71a0232f6e8cf
Alex Dabija <alex.dabija@gmail.com>
2022-03-18 13:01:25 +0100

    use view.width to set default width for nvim tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -2,11 +2,14 @@ if not pcall(require, 'nvim-tree') then
   return
 end
 
-vim.g.nvim_tree_width = 40
+-- vim.g.nvim_tree_width = 40
 vim.g.nvim_tree_indent_markers = 1
 
 require('nvim-tree').setup {
   auto_close = true,
+  view = {
+    width = 70,
+  },
   update_focused_file = {
     enable = true,
   }
