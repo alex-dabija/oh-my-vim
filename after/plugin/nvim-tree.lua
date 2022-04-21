ee22046688b114fb60fa27c510e735bcce3ec360
Alex Dabija <alex.dabija@gmail.com>
2022-04-21 16:11:52 +0200

    fix nvim-tree warning

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -2,11 +2,14 @@ if not pcall(require, 'nvim-tree') then
   return
 end
 
-vim.g.nvim_tree_indent_markers = 1
-
 require('nvim-tree').setup {
   view = {
     width = 70,
+  },
+  render = {
+    indent_markers = {
+      enable = true,
+    }
   },
   update_focused_file = {
     enable = true,
