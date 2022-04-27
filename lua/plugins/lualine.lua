73e7c5a51e45b629ed782d8a1fb2330112fca185
Alex Dabija <alex.dabija@gmail.com>
2022-04-27 14:58:03 +0200

    add lsp client to status line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,28 @@
 local M = {}
 
+local icons = {
+  locker         = '', -- f023
+  not_modifiable = '', -- f05e
+  unsaved        = '', -- f0c7
+  pencil         = '', -- f040
+  dos            = '', -- e70f
+  unix           = '', -- f17c
+  mac            = '', -- f179
+  page           = '☰', -- 2630
+  line_number    = '', -- e0a1
+  connected      = '', -- f817
+  disconnected   = '', -- f818
+  error          = '', -- f658
+  warning        = '', -- f071
+  hint           = '', -- f0eb
+  info           = '', -- f05a
+  git            = '', -- e725
+  git_add        = '', -- f2ba
+  git_modified   = '', -- f459
+  git_deleted    = '', -- f458
+  paste          = '', -- f691
+}
+
 local window_max_width = 70
 
 local function hide_if_max_width()
@@ -29,6 +52,24 @@ local diff = {
   cond = nil,
 }
 
+local lsp = {
+  function()
+    local buf_clients = vim.lsp.buf_get_clients()
+    if next(buf_clients) == nil then
+      return '' .. icons.disconnected .. ''
+    end
+
+    local buf_client_names = {}
+    for _, client in pairs(buf_clients) do
+      table.insert(buf_client_names, client.name)
+    end
+
+    return icons.connected .. '  ' .. table.concat(buf_client_names, ', ')
+  end,
+  -- color = { gui = 'bold' },
+  cond = hide_if_max_width,
+}
+
 function M.setup()
   require('lualine').setup {
     extensions = { 'quickfix', 'nvim-tree' },
@@ -36,10 +77,9 @@ function M.setup()
       lualine_a = { 'mode'},
       lualine_b = { 'filename', 'branch' },
       lualine_c = { diff, 'lsp_progress' },
-      lualine_x = { diagnostics },
+      lualine_x = { lsp, diagnostics },
       lualine_y = { 'encoding', 'fileformat', 'filetype' },
       lualine_z = { 'location' },
-
     },
   }
 end
