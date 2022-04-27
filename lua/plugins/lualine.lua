66a35902daba5826bff61cf24d58d151c54a5e9a
Alex Dabija <alex.dabija@gmail.com>
2022-04-27 14:34:41 +0200

    customize diagnostics

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,28 @@
 local M = {}
 
+local window_max_width = 70
+
+local function hide_if_max_width()
+  return vim.fn.winwidth(0) > window_max_width
+end
+
+local diagnostics = {
+  'diagnostics',
+  sources = { 'nvim_diagnostic' },
+  symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+  cond = hide_if_max_width,
+}
+
 function M.setup()
   require('lualine').setup {
     extensions = { 'quickfix', 'nvim-tree' },
     sections = {
-      lualine_a = {'mode'},
-      lualine_b = {'filename'},
-      lualine_c = {'lsp_progress'},
-      lualine_x = {},
-      lualine_y = {'encoding', 'fileformat', 'filetype'},
-      lualine_z = {'branch', 'diff', 'diagnostics', 'location'},
+      lualine_a = { 'mode'},
+      lualine_b = { 'filename', 'branch' },
+      lualine_c = { 'diff', 'lsp_progress' },
+      lualine_x = { diagnostics },
+      lualine_y = { 'encoding', 'fileformat', 'filetype' },
+      lualine_z = { 'location' },
 
     },
   }
