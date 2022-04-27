f45bf35dfaa0ef87367b336c73ecf0b82ca21548
Alex Dabija <alex.dabija@gmail.com>
2022-04-27 20:12:26 +0200

    test first keymaps configured with legendary

--- lua/plugins/legendary.lua
+++ lua/plugins/legendary.lua
@@ -1,6 +1,12 @@
 local M = {}
 
 function M.setup()
+  require('legendary').setup {
+    keymaps = {
+      { '<Leader>ve', ':edit $MYVIMRC<CR>', description = 'Edit VIM configuration', opts = {}},
+      { '<Leader>vs', ':luafile $MYVIMRC<CR>', description = 'Source VIM configuration', opts = {}},
+    }
+  }
 end
 
 return M
