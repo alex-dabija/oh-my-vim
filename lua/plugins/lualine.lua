f790a0e3aa8b68085b0d1b4d1f67e7e8f4f55413
Alex Dabija <alex.dabija@gmail.com>
2022-04-27 14:47:43 +0200

    lualine diff icons

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -13,13 +13,29 @@ local diagnostics = {
   cond = hide_if_max_width,
 }
 
+local diff = {
+  'diff',
+  source = function()
+    local gitsigns = vim.b.gitsigns_status_dict
+    if gitsigns then
+      return {
+        added = gitsigns.added,
+        modified = gitsigns.changed,
+        removed = gitsigns.removed,
+      }
+    end
+  end,
+  symbols = { added = '  ', modified = ' ', removed = ' ' },
+  cond = nil,
+}
+
 function M.setup()
   require('lualine').setup {
     extensions = { 'quickfix', 'nvim-tree' },
     sections = {
       lualine_a = { 'mode'},
       lualine_b = { 'filename', 'branch' },
-      lualine_c = { 'diff', 'lsp_progress' },
+      lualine_c = { diff, 'lsp_progress' },
       lualine_x = { diagnostics },
       lualine_y = { 'encoding', 'fileformat', 'filetype' },
       lualine_z = { 'location' },
