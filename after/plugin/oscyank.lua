dee7fc2ac5e81c3d80e1b1ccd3fb2d2d28ee29ee
Alex Dabija <alex.dabija@gmail.com>
2022-09-22 20:00:47 +0200

    move oscyank config to after directory

---
+++ after/plugin/oscyank.lua
@@ -0,0 +1,6 @@
+vim.api.nvim_exec([[
+  augroup osc_yank
+    autocmd!
+    autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankReg "' | endif
+  augroup end
+]], false)
