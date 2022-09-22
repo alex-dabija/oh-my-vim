fcab49c4a318ba4fb0b62260b8e6ec004843dd33
Alex Dabija <alex.dabija@gmail.com>
2022-09-22 19:57:04 +0200

    remove plugins without setup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,64 +13,11 @@ packer.startup(function(use)
   -- Package manager
   use {'wbthomason/packer.nvim'}
 
-  -- Common
-  use {'nvim-lua/popup.nvim'}
-  use {'nvim-lua/plenary.nvim'}
-  use {'kyazdani42/nvim-web-devicons'}
-
   -- LSP
   use {
     'neovim/nvim-lspconfig',
     config = function() require('plugins.lsp-config').setup() end
   }
-  use {'kosayoda/nvim-lightbulb'}
-  -- use {
-  --   'simrat39/rust-tools.nvim',
-  --   branch = 'modularize_and_inlay_rewrite',
-  --   config = function() require('plugins.rust-tools').setup() end
-  -- }
-
-  -- Autocomplete
-  use {'hrsh7th/nvim-cmp'}
-  use {'hrsh7th/cmp-nvim-lsp'}
-
-  -- Snippets
-  use {'saadparwaiz1/cmp_luasnip'}
-  use {'L3MON4D3/LuaSnip'}
-
-  -- Treesitter
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-  use {'nvim-treesitter/nvim-treesitter-refactor'}
-  use {'nvim-treesitter/nvim-treesitter-textobjects'}
-  use {'nvim-treesitter/playground'}
-
-  -- Telescope
-  use {'nvim-telescope/telescope.nvim'}
-  use {'nvim-telescope/telescope-ui-select.nvim'}
-  use {'TC72/telescope-tele-tabby.nvim'}
-
-  -- Session manager
-  use {'Shatur/neovim-session-manager'}
-
-  -- Colorschemes
-  -- use {
-  --   'dracula/vim',
-  --   as = 'dracula',
-  --   config = function() require('utils').set_colorscheme('dracula') end
-  -- }
-
-  use {
-    'folke/tokyonight.nvim',
-    after = 'lualine.nvim',
-    -- config = function() require('utils').set_colorscheme('tokyonight') end
-  }
-  use {
-    'EdenEast/nightfox.nvim',
-    after = 'lualine.nvim',
-    config = function() require('utils').set_colorscheme('nightfox') end
-  }
-
-  use {'kyazdani42/nvim-tree.lua'}
 
   -- UI plugins
   use {
@@ -89,13 +36,6 @@ packer.startup(function(use)
     'arkav/lualine-lsp-progress',
   }
 
-  -- Git
-  use {'tpope/vim-fugitive'}
-  use {'tpope/vim-rhubarb'}
-  use {'lewis6991/gitsigns.nvim'}
-
-  use {'numToStr/Comment.nvim'}
-
   use {
     'norcalli/nvim-colorizer.lua',
     config = function()
@@ -103,17 +43,11 @@ packer.startup(function(use)
     end,
   }
 
-  use {'phaazon/hop.nvim'}
-
   use {
     'ojroques/vim-oscyank',
     config = function() require('plugins.oscyank').setup() end
   }
 
-  use {'AckslD/nvim-neoclip.lua'}
-
-  use { 'tpope/vim-surround' }
-  use { 'tpope/vim-repeat' }
   use {
     'windwp/nvim-autopairs',
     config = function() require('nvim-autopairs').setup() end
