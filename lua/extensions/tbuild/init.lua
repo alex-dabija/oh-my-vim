f2ee7a761fe4c92cc96848a314e2a17418b60616
Alex Dabija <alex.dabija@gmail.com>
2022-09-23 20:06:54 +0200

    remove unsued tbuil extension

--- lua/extensions/tbuild/init.lua
+++
@@ -1,9 +0,0 @@
-local Terminal = require('extensions.tbuild.terminal')
-local Window = require('extensions.tbuild.window')
-local DefaultTerminal = Terminal:new(Window:new())
-
-return {
-  Terminal = Terminal,
-  Window = Window,
-  DefaultTerminal = DefaultTerminal,
-}
