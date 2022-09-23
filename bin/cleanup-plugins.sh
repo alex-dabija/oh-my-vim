12c4ba65d25511a60ed8745b00d02469ccd31bc2
Alex Dabija <alex.dabija@gmail.com>
2022-09-23 20:05:55 +0200

    remove unused cleaup-plugins.sh plugin

--- bin/cleanup-plugins.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-set -o nounset
-
-rm -f "${HOME}/.config/nvim/plugin/packer_compiled.vim"
-rm -rf "${HOME}/.local/share/nvim/site/oh-my-vim/"
-rm -rf "${HOME}/.local/share/nvim/site/pack/"
