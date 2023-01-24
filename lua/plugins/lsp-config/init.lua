ec1a6b3b4d4de6beef206977dcef66e5739e83da
Alex Dabija <1087557+alex-dabija@users.noreply.github.com>
2023-01-24 21:27:47 +0100

    configure rust_analyzer only with rust-tools

--- lua/plugins/lsp-config/init.lua
+++ lua/plugins/lsp-config/init.lua
@@ -52,7 +52,7 @@ function M.setup()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-  local servers = { 'rust_analyzer', 'gopls', 'pylsp', 'ansiblels', 'jdtls' }
+  local servers = { 'gopls', 'pylsp', 'ansiblels', 'jdtls' }
   for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
       on_attach = configure_buffer,
